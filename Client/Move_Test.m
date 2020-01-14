e3dd4013ce75319dc7e9a87b79d5fcd3745f645b
andre <brauners@hm.edu>
2020-01-14 13:40:03 +0100

    add test script for moving

---
+++ Client/Move_Test.m
@@ -0,0 +1,23 @@
+close all;
+addpath('..\..\ARIA_2.9.1 (64-bit)_matlab_precompiled');
+clear all
+addpath('..\..\ARIA_2.9.1 (64-bit)_matlab_precompiled');
+
+load('+data\sensorPose.mat')
+try
+    % Initialisierung
+    utils.init_robot('localhost', '8101');
+    
+    
+    % Main control loop. End with Ctrl + C in command line.
+    target = [1000 0];
+    robot_controls.move_to_target(target, sensorPose);
+    
+    arrobot_disconnect
+    
+catch err
+    disp 'error or cancelled'
+    disp(err)
+    arrobot_stop
+    arrobot_disconnect
+end
