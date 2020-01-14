e1dc7afb5e3358e0ab213754f649ab7b469c0860
Nerolf05 <florian.roessl57@gmail.com>
2020-01-14 10:31:34 +0100

    Steuerung

--- HM_TA_Aria_getting_Started.m
+++ HM_TA_Aria_getting_Started.m
@@ -15,24 +15,33 @@ ver
 %% Aria Pfad und Neustart
 % ==> notwendiger work around
 % pfad hinzufügen
-addpath('C:\HM\prj\MobileRobots\ARIA_2.9.1 (64-bit)_matlab_precompiled');
+addpath('C:\Users\Sysadmin\Desktop\PS2020\ARIA_2.9.1 (64-bit)_matlab_precompiled');
 
 % disconnet robot
 arrobot_disconnect
 % nochmals clear notwendig
 clear all
 % pfad hinzufügen
-addpath('C:\HM\prj\MobileRobots\ARIA_2.9.1 (64-bit)_matlab_precompiled');
+addpath('C:\Users\Sysadmin\Desktop\PS2020\ARIA_2.9.1 (64-bit)_matlab_precompiled');
 % <==
 
 %% connect to the robot:
-aria_init -rh 10.22.192.161  -rrtp 8102
+aria_init -rh localhost  -rrtp 8101
 arrobot_connect
 
 
 %% make the robot drive in a small circle:
-arrobot_setvel(300)
-arrobot_setrotvel(35)
+% arrobot_setvel(300)
+% arrobot_setrotvel(35)
+
+
+%% Sonar einlesen
+% sonar = load('Sonar.txt');
+
+%%Aktuelle Postition des Roboters abfragen
+x = arrobot_getx();
+y = arrobot_gety();
+
 
 %% Position abfragen
 figure(1);
@@ -44,12 +53,55 @@ for i=1:10
     pause(0.5);
 end
 
+
 %% simple Steuerung
-% reply = input('Enter control command \r\n', 's');
-% switch reply
-%     case 'i'         % i
-%         break;
-% end
+while (true)
+reply = input('Enter control command \r\n', 's');
+ switch reply
+     
+     %%Vorwärts
+     case 'w'         % i
+         arrobot_setvel(800);
+         pause(3);
+         arrobot_stop;
+         
+     %%Rückwärts    
+     case 's'
+         arrobot_setvel(-500);
+         pause(1);
+         arrobot_stop;
+       
+     %%Links    
+     case 'a'
+         arrobot_setdeltaheading(90);
+            pause(1.5);
+     %%Rechts    
+     case 'd'
+         arrobot_setdeltaheading(-90);
+            pause(1);
+        
+     case 'q'
+         arrobot_stop;
+            pause(1);
+         
+     case 'c'
+         t = false;
+     case 'p'
+         figure(2);
+         show(map)
+         hold on;
+         
+     case 'x'
+         arrobot_stop;
+%          goHome
+         
+     case 'y'
+         break;
+         
+    
+ end
+end
+%  counter = counter+1;
 
 %% pause und disconnect
 pause(2);
