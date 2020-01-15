97cc5438451f3b39a7470d531cf47de59b1ec74d
andre <brauners@hm.edu>
2020-01-15 16:10:45 +0100

    fix bug in calculation of the sensed points. change get_sensorreadings to get_sensorreadings_worldframe

--- Client/+robot_controls/get_sensorreadings.m
+++ Client/+robot_controls/get_sensorreadings.m
@@ -1,4 +1,4 @@
-function [pointcloud] = get_sensorreadings(sensorPose)
+function [pointcloud, pose] = get_sensorreadings(sensorPose)
 %GET_SENSORREADINGS Reads the sonar sensor and returns a pointcloud of
 %detected objects around the robot in body coordinate system of the robot
 %Unit is in mm
@@ -6,29 +6,28 @@ function [pointcloud] = get_sensorreadings(sensorPose)
 n = length(sensorPose);
 
 pointcloud = [];
+pose = [];
 sensor_values = zeros(n, 1);
 
 % Read each sensor measurement
 for i = 1 : n
     val = arrobot_getsonarrange(i-1);
     if val ~= 5000
-        % Read estimated robot position
-        rx = arrobot_getx;
-        ry = arrobot_gety;
-        th = arrobot_getth;
-        
-        [x, y] = koppel(rx, ry, th, val, sensorPose(i, 3));
+        rx = arrobot_getx
+        ry = arrobot_gety
+        th = arrobot_getth
+        [x, y] = polar_to_kart(val, sensorPose(i, 3));
         % Apply correct position of the sensor
         x = x + sensorPose(i, 1)*1000;
         y = y + sensorPose(i, 2)*1000;
         pointcloud = [pointcloud; [x, y]];
+        %Save the current pose to each measurement for further calculations
+        pose = [pose; [rx, ry, th]];
     end
 end
 end
 
-function [x, y] = koppel(posx, posy, theta, dist, angle)
-x = posx + dist * cosd(angle+theta);
-y = posy + dist * sind(angle+theta);
+function [x, y] = polar_to_kart(dist, angle)
+x = dist * cosd(angle);
+y = dist * sind(angle);
 end
-
-
